f502860ca047b21edb2bf0612de349a2a100a524
Jiaxing.Bai <jiaxing.bai@centurygame.com>
2021-08-09 02:26:31 +0800

    UI分层管理组件

---
+++ TimeUnity/Assets/Scripts/View/RoomLayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TimeUnity.View{
+    public class RoomLayer : MonoBehaviour
+    {
+        public List<Room> rooms;
+        public MainCharacter MainCharacter;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
---
+++ TimeUnity/Assets/Scripts/View/UILayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TimeUnity.View{
+    public class UILayer : MonoBehaviour
+    {
+        public GameObject layout;
+        public DialogPopup PopupButtonTip;
+        public GameObject PopupDialog;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
